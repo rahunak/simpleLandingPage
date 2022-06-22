7fab68720bd4584c7025e44185fc5a3b7fd56702
Zaiko Euheni <rahunak@yandex.by>
2022-06-23 01:08:06 +0300

    feat: did almost all styles LANG=en_EN date +(%a, %b %d,%Y %r)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import 'normalize.css';
 
 // styles
+import './components/scss/fonts.scss';
+import './components/scss/common.scss';
 import './components/scss/main.scss';
+import './components/scss/header/header.scss';
