c4c52a12ee122f1f1aae530f802ca8a79747d12b
unknown <66bratz@gmail.com>
2022-06-27 16:34:02 +0300

    add some style

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import 'normalize.css';
 import './components/scss/fonts.scss';
 import './components/scss/common.scss';
 import './components/scss/main.scss';
-import './components/scss/burger.scss';
+import './components/scss/header/burger.scss';
 import './components/scss/header/header.scss';
 import './components/scss/buttons/buttonsMainPage.scss';
+import './components/scss/footer/footerInfo.scss';
 // media rule
 import './components/scss/media.scss';
 // scripts
